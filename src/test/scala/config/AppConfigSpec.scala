2c45b8a7408d2ba4bda51f1242c21fa8071eebd4
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2025-10-25 13:16:36 +0300

    Fix: Format code to fix linting issues

--- src/test/scala/config/AppConfigSpec.scala
+++ src/test/scala/config/AppConfigSpec.scala
@@ -1,16 +1,18 @@
 package config
 
-import zio.test.* 
+import zio.test.*
 import zio.config.typesafe.TypesafeConfigProvider
 import zio.{Scope, ZIO}
 
-object AppConfigSpec extends ZIOSpecDefault {
+object AppConfigSpec extends ZIOSpecDefault:
 
-  override def spec: Spec[TestEnvironment & Scope, Any] = suite("AppConfigSpec")(
-    test("load from application.conf") {
-      val configProvider = TypesafeConfigProvider.fromResourcePath()
-      val config = configProvider.load(AppConfig.descriptor)
-      assertZIO(config.map(_.sink.sinkType))(Assertion.equalTo(SinkType.Console))
-    }
-  )
-}
+  override def spec: Spec[TestEnvironment & Scope, Any] =
+    suite("AppConfigSpec")(
+      test("load from application.conf") {
+        val configProvider = TypesafeConfigProvider.fromResourcePath()
+        val config = configProvider.load(AppConfig.descriptor)
+        assertZIO(config.map(_.sink.sinkType))(
+          Assertion.equalTo(SinkType.Console)
+        )
+      }
+    )
